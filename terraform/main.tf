6bc9d3db2c40dd3843c82692e4f3e0a7b43074be
romain <romain-l58@hotmail.fr>
2020-02-06 12:48:34 +0100

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   client_id       = var.client_id
-  client_secret   = var.cl
+  client_secret   = var.client_secret
 }
 
 
