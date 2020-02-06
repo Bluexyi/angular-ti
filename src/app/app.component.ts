9d95c72009eeb40d395f163a4710d085241ee505
romain <romain-l58@hotmail.fr>
2020-02-06 13:14:15 +0100

    correctif bug test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Angular dev environnement v0';
+  title = 'Angular Unit Testing';
 }
